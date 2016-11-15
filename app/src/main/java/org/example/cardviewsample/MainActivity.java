6f86a5d856007637413fd720e0ab28c4393df934
ksugawara61 <katsuo.com61@gmail.com>
2016-11-16 00:08:50 +0900

    http://qiita.com/briete/items/b7ee295977be16f97c58 の内容を追記

--- app/src/main/java/org/example/cardviewsample/MainActivity.java
+++ app/src/main/java/org/example/cardviewsample/MainActivity.java
@@ -2,6 +2,12 @@ package org.example.cardviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout cardLinear = (LinearLayout)this.findViewById(R.id.cardLinear);
+        cardLinear.removeAllViews();
+
+        for(int i = 0; i< 5; i++) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.test_card, null);
+            CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
+            TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
+            textBox.setText("CardView" + i);
+            cardView.setTag(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, String.valueOf(v.getTag()) + "番目のCardViewがクリックされました", Toast.LENGTH_SHORT).show();
+                }
+            });
+            cardLinear.addView(linearLayout,i);
+        }
     }
 }
